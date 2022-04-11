4807bb2f1005e7fd03cdcac953957b43eca30be9
Juboskar <oskari.juvakka@gmail.com>
2022-04-11 15:14:14 +0300

    2.12

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,7 +1,66 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const CountryList = (props) => {
+  if (props.countryList.length === 1) {
+    const c = props.countryList[0]
+    const l = Object.values(c.languages)
+
+    return (
+      <div>
+        <h1>
+          {c.name.common}
+        </h1>
+        <div>capital {c.capital}</div>
+        <div>area {c.area}</div>
+        <h4>languages:</h4>
+        <ul>
+          {l.map(x => <li>{x}</li>)}
+        </ul>
+        <img src={c.flags.svg} width="300" />
+      </div>
+    )
+  } else if (props.countryList.length <= 10) {
+    return (
+      <div>
+        {props.countryList.map(c =>
+          <div key={c.name.common}>{c.name.common}</div>
+        )}
+      </div>
+    )
+  } else {
+    return (
+      <div>Too many matches, specify another filter</div>
+    )
+  }
+}
+
 const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  })
+
+  const handleValueChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const countriesToShow =
+    countries.filter(countries => countries.name.common.toLowerCase().includes(filter))
+
   return (
     <div>
-      Hello
+      <label>
+        Find countries
+        <input onChange={handleValueChange} />
+      </label>
+      <CountryList countryList={countriesToShow} />
     </div>
   );
 }
