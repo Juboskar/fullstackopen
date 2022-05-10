70c09c0fee865b3849e4e143517dde70d174fe44
Juboskar <oskari.juvakka@gmail.com>
2022-05-10 22:12:31 +0300

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return Object.keys(blogs).reduce((sum, key) => {
+    return sum + blogs[key].likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
