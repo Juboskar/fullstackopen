90e691bec7585e9d99704b6d38a65e79cbd3285f
Juboskar <oskari.juvakka@gmail.com>
2022-06-13 16:59:45 +0300

    4.19

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const loginRouter = require('./controllers/login')
+const errorHandler = require('./utils/errorHandler')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -17,5 +18,8 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use(errorHandler)
+
+
 
 module.exports = app
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,41 +1,56 @@
 const blogsRouter = require('express').Router()
 const { result } = require('lodash')
+const jwt = require('jsonwebtoken')
+
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user',  { username: 1, name: 1, id: 1 })
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
 
-  if (blog.title === undefined) {
-    return response.status(400).json({ error: 'title missing' })
-  }
+  const token = getTokenFrom(request)
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
-  if (blog.author === undefined) {
-    return response.status(400).json({ error: 'author missing' })
-  }
 
-  if (blog.likes === undefined) {
-    blog.likes = 0
-  }
+    if (blog.title === undefined) {
+      return response.status(400).json({ error: 'title missing' })
+    }
 
-  // temporary way of finding user id
-  const users = await User.find({})
+    if (blog.author === undefined) {
+      return response.status(400).json({ error: 'author missing' })
+    }
 
-  const userId = users[0]._id
-  blog.user = userId
-  const result = await blog.save()
+    if (blog.likes === undefined) {
+      blog.likes = 0
+    }
 
-  const user = await User.findById(userId)
-  user.blogs = user.blogs.concat(result._id)
-  await user.save()
 
-  return response.status(201).json(result)
+    const user = await User.findById(decodedToken.id)
+    blog.user = user.id
+    const result = await blog.save()
 
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
+
+    return response.status(201).json(result)
+  } catch (error) { next(error) }
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
