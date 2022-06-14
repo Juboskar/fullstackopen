7f418ccd3a8504c61f8f8dec0637f06be3b8e4eb
Juboskar <oskari.juvakka@gmail.com>
2022-06-14 23:19:00 +0300

    5.18

--- osa5/bloglist-frontend/cypress/e2e/blog_app.spec.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.spec.cy.js
@@ -1,13 +1,38 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      username: 'testuser',
+      name: 'Testi Usersson',
+      password: 'testpassword'
+    })
+
+
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.contains('Login')
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
   })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('testpassword')
+      cy.get('#login-button').click()
+      cy.contains('testuser logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('test')
+      cy.get('#password').type('test')
+      cy.get('#login-button').click()
+      cy.contains('wrong credentials')
+    })
+  })
 })
+
